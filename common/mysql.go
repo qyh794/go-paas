031a141015e1f6b9da7cdf89b81122836a5710ac
qyh794 <727280876@qq.com>
2023-03-31 15:42:54 +0800

    Update mysql.go

--- common/mysql.go
+++ common/mysql.go
@@ -1,6 +1,9 @@
 package common
 
-import "github.com/asim/go-micro/v3/config"
+import (
+	"github.com/asim/go-micro/v3/config"
+	"github.com/jinzhu/gorm"
+)
 
 type MysqlConfig struct {
 	Host     string `json:"host"`
@@ -14,4 +17,12 @@ func GetMysqlConfigFromConsul(config config.Config, path ...string) *MysqlConfig
 	mysqlConfig := &MysqlConfig{}
 	config.Get(path...).Scan(mysqlConfig)
 	return mysqlConfig
+}
+
+func InitMySQL(dialect string, mysqlConfig *MysqlConfig) (*gorm.DB, error) {
+	db, err := gorm.Open(dialect, mysqlConfig.User+":"+mysqlConfig.Password+"@("+mysqlConfig.Host+":3306)/"+mysqlConfig.Database+"?charset=utf8&parseTime=True&loc=Local")
+	if err != nil {
+		return nil, err
+	}
+	return db, err
 }
