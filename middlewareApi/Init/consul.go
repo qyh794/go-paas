085f915c79bfd08cc6720133a36e0156693f4daf
qyh794 <727280876@qq.com>
2023-04-14 00:05:06 +0800

    Create consul.go

---
+++ middlewareApi/Init/consul.go
@@ -0,0 +1,16 @@
+package Init
+
+import (
+	"github.com/asim/go-micro/plugins/registry/consul/v3"
+	"github.com/asim/go-micro/v3/registry"
+	"strconv"
+)
+
+func InitConsul(consulHost string, consulPort int) registry.Registry {
+	newConsul := consul.NewRegistry(func(options *registry.Options) {
+		options.Addrs = []string{
+			consulHost + ":" + strconv.Itoa(consulPort),
+		}
+	})
+	return newConsul
+}
