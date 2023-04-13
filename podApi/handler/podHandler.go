0d0b3966184a303a06debd50c0397bfc4d54a71b
qyh794 <727280876@qq.com>
2023-04-14 00:05:30 +0800

    Update podHandler.go

--- podApi/handler/podHandler.go
+++ podApi/handler/podHandler.go
@@ -7,7 +7,6 @@ import (
 	"fmt"
 	"github.com/asim/go-micro/v3/logger"
 	"github.com/qyh794/go-paas/pod/proto/pod"
-	"github.com/qyh794/go-paas/podApi/plugin/form"
 	"github.com/qyh794/go-paas/podApi/proto/podApi"
 	"strconv"
 )
@@ -54,25 +53,13 @@ func (p *PodApi) AddPod(ctx context.Context, request *podApi.Request, response *
 	fmt.Println("接受到 podApi.AddPod请求")
 	// 请求中带有多个端口号,需要获取然后再添加到podInfo中
 	podInfoObj := &pod.RPodInfo{}
-	portVal, ok := request.Get["pod_port"]
-	if ok {
-		var podPorts []*pod.PodPort
-		for i, _ := range portVal.Values {
-			// pair.Values为string类型
-			portInt, err := strconv.ParseInt(portVal.Values[i], 10, 32)
-			if err != nil {
-				logger.Error(err)
-			}
-			port := &pod.PodPort{
-				ContainerPort: int32(portInt),
-				Protocol:      "TCP",
-			}
-			podPorts = append(podPorts, port)
-		}
-		podInfoObj.PodPort = podPorts
+	// 从请求体中获取参数,将其反序列化到结构体中
+	err := json.Unmarshal([]byte(request.Body), podInfoObj)
+	if err != nil {
+		response.StatusCode = 403
+		response.Body = err.Error()
+		return err
 	}
-	// 将请求中的表单内容转化到结构体中
-	form.FromToPodStruct(request.Post, podInfoObj)
 	// 调用pod.AddPod
 	responseInfo, err := p.PodService.AddPod(ctx, podInfoObj)
 	if err != nil {
@@ -88,7 +75,7 @@ func (p *PodApi) AddPod(ctx context.Context, request *podApi.Request, response *
 // DeletePodByID podApi.DeletePodById 通过API向外暴露为/podApi/deletePodById，接收http请求
 func (p *PodApi) DeletePodByID(ctx context.Context, request *podApi.Request, response *podApi.Response) error {
 	fmt.Println("接收到podApi.DeletePodByID的请求")
-	// 从请求中获取podID
+	// 从请求url中获取podID
 	val, ok := request.Get["pod_id"]
 	if !ok {
 		return errors.New("参数异常")
@@ -120,24 +107,12 @@ func (p *PodApi) UpdatePod(ctx context.Context, request *podApi.Request, respons
 	fmt.Println("接收到podApi.UpdatePod请求")
 	podObj := &pod.RPodInfo{}
 	// 处理请求中port信息
-	portVal, ok := request.Get["pod_port"]
-	if ok {
-		var portSlice []*pod.PodPort
-		for i, _ := range portVal.Values {
-			portInt, err := strconv.ParseInt(portVal.Values[i], 10, 32)
-			if err != nil {
-				logger.Error(err)
-			}
-			port := &pod.PodPort{
-				ContainerPort: int32(portInt),
-				Protocol:      "TCP",
-			}
-			portSlice = append(portSlice, port)
-		}
-		podObj.PodPort = portSlice
+	err := json.Unmarshal([]byte(request.Body), podObj)
+	if err != nil {
+		response.StatusCode = 403
+		response.Body = err.Error()
+		return err
 	}
-	// 将请求中的数据映射到pod.RPodInfo结构体中
-	form.FromToPodStruct(request.Post, podObj)
 	// 调用UpdatePod服务
 	responseInfo, err := p.PodService.UpdatePod(ctx, podObj)
 	if err != nil {
@@ -152,7 +127,7 @@ func (p *PodApi) UpdatePod(ctx context.Context, request *podApi.Request, respons
 
 // Call 默认的方法podApi.Call 通过API向外暴露为/podApi/call，接收http请求
 func (p *PodApi) Call(ctx context.Context, request *podApi.Request, response *podApi.Response) error {
-	fmt.Println("接收到podApi.UpdatePod请求")
+	fmt.Println("接收到podApi.QueryAll 请求")
 	pods, err := p.PodService.QueryAllPods(ctx, &pod.RequestQueryAll{})
 	if err != nil {
 		logger.Error(err)
