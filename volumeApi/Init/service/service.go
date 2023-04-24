99fe6932125738fd677589c5b3f0bcbda96e7929
qyh794 <727280876@qq.com>
2023-04-24 13:00:46 +0800

    Create service.go

---
+++ volumeApi/Init/service/service.go
@@ -0,0 +1,25 @@
+package service
+
+import (
+	ratelimit "github.com/asim/go-micro/plugins/wrapper/ratelimiter/uber/v3"
+	"github.com/asim/go-micro/v3"
+	"github.com/asim/go-micro/v3/registry"
+	"github.com/asim/go-micro/v3/server"
+)
+
+const QPS = 1000
+
+func Init(serverHost, serverPort, serverName, serverVersion string, consul registry.Registry) micro.Service {
+	newService := micro.NewService(
+		micro.Server(server.NewServer(func(options *server.Options) {
+			options.Advertise = serverHost + ":" + serverPort
+		})),
+		micro.Name(serverName),
+		micro.Version(serverVersion),
+		micro.Address(":"+serverPort),
+		micro.Registry(consul),
+		micro.WrapHandler(ratelimit.NewHandlerWrapper(QPS)),
+	)
+	newService.Init()
+	return newService
+}
