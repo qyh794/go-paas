6dc7abd9b6b437a4c3dd9f858b87d89b50550292
qyh794 <727280876@qq.com>
2023-04-24 13:09:42 +0800

    Create k8s.go

---
+++ middleware/Init/k8s/k8s.go
@@ -0,0 +1,30 @@
+package k8s
+
+import (
+	"flag"
+	"github.com/asim/go-micro/v3/logger"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
+	"path/filepath"
+)
+
+func Init() *kubernetes.Clientset {
+	var kubeconfig *string
+	if home := homedir.HomeDir(); home != "" {
+		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+	} else {
+		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+	}
+	flag.Parse()
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	if err != nil {
+		logger.Fatal(err.Error())
+	}
+
+	clientSet, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		logger.Fatal(err.Error())
+	}
+	return clientSet
+}
