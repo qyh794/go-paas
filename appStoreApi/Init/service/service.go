7d9f03c9283cf4b0732e2beb38593bc2283e8b6c
qyh794 <727280876@qq.com>
2023-04-24 12:10:29 +0800

    Create service.go

---
+++ appStoreApi/Init/service/service.go
@@ -0,0 +1,27 @@
+package service
+
+import (
+	"appStoreApi/hystrix"
+	ratelimit "github.com/asim/go-micro/plugins/wrapper/ratelimiter/uber/v3"
+	"github.com/asim/go-micro/v3"
+	"github.com/asim/go-micro/v3/registry"
+	"github.com/asim/go-micro/v3/server"
+)
+
+const QPS = 1000
+
+func Init(serverHost, serverPort, serverName, serverVersion string, consul registry.Registry) micro.Service {
+	newService := micro.NewService(
+		micro.Server(server.NewServer(func(options *server.Options) {
+			options.Advertise = serverHost + ":" + serverPort
+		})),
+		micro.Name(serverName),
+		micro.Version(serverVersion),
+		micro.Address(":"+serverPort),
+		micro.Registry(consul),
+		micro.WrapClient(hystrix.NewClientHystrixWrapper()),
+		micro.WrapHandler(ratelimit.NewHandlerWrapper(QPS)),
+	)
+	newService.Init()
+	return newService
+}
