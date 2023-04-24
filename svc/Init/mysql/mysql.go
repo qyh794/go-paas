a31a2e2970f45cf6ebcdf0e9e307f9fc33627d3c
qyh794 <727280876@qq.com>
2023-04-24 12:58:34 +0800

    Create mysql.go

---
+++ svc/Init/mysql/mysql.go
@@ -0,0 +1,29 @@
+package mysql
+
+import (
+	"fmt"
+	"github.com/jinzhu/gorm"
+	"github.com/qyh794/go-paas/common"
+)
+
+var DB *gorm.DB
+
+func Init(dialect string, mysqlConfig *common.MysqlConfig) (err error) {
+	dsn := fmt.Sprintf("%s:%s@(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local",
+		mysqlConfig.User,
+		mysqlConfig.Password,
+		mysqlConfig.Host,
+		mysqlConfig.Port,
+		mysqlConfig.Database,
+	)
+	DB, err = gorm.Open(dialect, dsn)
+	if err != nil {
+		return
+	}
+	DB.SingularTable(true)
+	return
+}
+
+func Close() {
+	_ = DB.Close()
+}
