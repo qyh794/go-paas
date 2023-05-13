7051b0a54a70d79823f55d117ac608cfeadd6ea9
qyh794 <727280876@qq.com>
2023-05-13 19:53:20 +0800

    Update main.go

--- user/main.go
+++ user/main.go
@@ -14,23 +14,33 @@ import (
 )
 
 func main() {
+	// 初始化viper
 	if err := settings.Init(); err != nil {
 		return
 	}
+	// 初始化consul
 	consul := consulInit.Init(settings.Conf.Consul.Host, settings.Conf.Consul.Port)
+
 	consulConfig, err := common.GetConsulConfig(settings.Conf.Consul.Host, settings.Conf.Consul.Port, settings.Conf.Consul.Prefix)
 	if err != nil {
 		logger.Error(err)
 	}
+
+	// 获取MySQL配置
 	mysqlConfig := common.GetMysqlConfigFromConsul(consulConfig, settings.Conf.Consul.Path)
 	if err = mysql.Init("mysql", mysqlConfig); err != nil {
 		logger.Error(err)
 	}
 	defer mysql.Close()
+
+	//初始化service
 	service := serviceInit.Init(settings.Conf.ServiceHost, settings.Conf.ServicePort, settings.Conf.Name, settings.Conf.Version, consul)
 	service.Init()
+
 	userDataService := service2.NewUserDataService(repository.NewUserRepository(mysql.DB))
+	// 注册函数
 	_ = user.RegisterUserHandler(service.Server(), &handler.UserHandler{UserDataService: userDataService})
+	// 运行service
 	if err = service.Run(); err != nil {
 		logger.Error(err)
 	}
