f3b22548463a496d497fd9814ce6e686709c55cf
qyh794 <727280876@qq.com>
2023-05-14 23:00:37 +0800

    Update userApiHandler.go

--- userApi/handler/userApiHandler.go
+++ userApi/handler/userApiHandler.go
@@ -3,7 +3,6 @@ package handler
 import (
 	"context"
 	"encoding/json"
-	"github.com/asim/go-micro/v3/logger"
 	"github.com/qyh794/go-paas/user/proto/user"
 	"github.com/qyh794/go-paas/userApi/proto/userApi"
 )
@@ -16,39 +15,26 @@ func (u *UserApi) SignUp(ctx context.Context, request *userApi.Request, response
 	userInfo := &user.RequestSignUp{}
 	err := json.Unmarshal([]byte(request.Body), userInfo)
 	if err != nil {
-		logger.Error(err)
-		response.Body = err.Error()
-		return err
+		return ResponseBadRequest(ctx, err, response)
 	}
 
 	responseSignUp, err := u.UserService.SignUp(ctx, userInfo)
 	if err != nil {
-		logger.Error(err)
-		response.Body = err.Error()
-		return err
+		return ResponseServerError(ctx, err, response)
 	}
-	response.StatusCode = 200
-	bytes, _ := json.Marshal(responseSignUp)
-	response.Body = string(bytes)
-	return nil
+	return ResponseSucceed(ctx, responseSignUp, response)
 }
 
 func (u *UserApi) Login(ctx context.Context, request *userApi.Request, response *userApi.Response) error {
 	userInfo := &user.RequestLogin{}
 	err := json.Unmarshal([]byte(request.Body), userInfo)
 	if err != nil {
-		response.StatusCode = 403
-		response.Body = err.Error()
-		return err
+		return ResponseBadRequest(ctx, err, response)
 	}
 	responseLogin, err := u.UserService.Login(ctx, userInfo)
 	if err != nil {
-		logger.Error(err)
-		response.Body = err.Error()
-		return err
+		return ResponseServerError(ctx, err, response)
+
 	}
-	response.StatusCode = 200
-	bytes, _ := json.Marshal(responseLogin)
-	response.Body = string(bytes)
-	return nil
+	return ResponseSucceed(ctx, responseLogin, response)
 }
