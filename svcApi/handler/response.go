035288240aff17ad7a2d0b928084dbdc793eb8cc
qyh794 <727280876@qq.com>
2023-05-14 23:00:23 +0800

    Create response.go

---
+++ svcApi/handler/response.go
@@ -0,0 +1,40 @@
+package handler
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"github.com/qyh794/go-paas/svcApi/proto/svcApi"
+	"net/http"
+)
+
+func ResponseSucceed(ctx context.Context, msg interface{}, response *svcApi.Response) error {
+	response.StatusCode = http.StatusOK
+	bytes, _ := json.Marshal(msg)
+	response.Body = string(bytes)
+	return nil
+}
+
+func ResponseAuthFailed(ctx context.Context, err error, response *svcApi.Response) error {
+	response.StatusCode = http.StatusNetworkAuthenticationRequired
+	response.Body = err.Error()
+	return err
+}
+
+func ResponseBadRequest(ctx context.Context, err error, response *svcApi.Response) error {
+	response.StatusCode = http.StatusBadRequest
+	response.Body = err.Error()
+	return err
+}
+
+func ResponseServerError(ctx context.Context, err error, response *svcApi.Response) error {
+	response.StatusCode = http.StatusInternalServerError
+	response.Body = err.Error()
+	return err
+}
+
+func ResponseEmptyToken(ctx context.Context, response *svcApi.Response) error {
+	response.StatusCode = http.StatusNetworkAuthenticationRequired
+	response.Body = errors.New("empty token").Error()
+	return errors.New("empty token")
+}
