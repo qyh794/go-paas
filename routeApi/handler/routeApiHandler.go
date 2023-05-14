9e70927d94b817fc2e50027e5472556e69ef0653
qyh794 <727280876@qq.com>
2023-05-14 23:00:19 +0800

    Update routeApiHandler.go

--- routeApi/handler/routeApiHandler.go
+++ routeApi/handler/routeApiHandler.go
@@ -5,162 +5,122 @@ import (
 	"encoding/json"
 	"errors"
 	"github.com/asim/go-micro/v3/util/log"
-	"github.com/micro/micro/v3/service/logger"
 	"github.com/qyh794/go-paas/route/proto/route"
 	"github.com/qyh794/go-paas/routeApi/pkg/jwt"
 	"github.com/qyh794/go-paas/routeApi/proto/routeApi"
 	"strconv"
-	"strings"
 )
 
 type RouteApi struct {
 	RouteService route.RouteService
 }
 
-const (
-	Succeed     = 200
-	WrongArgs   = 400
-	ServiceBusy = 500
-)
-
 func (r *RouteApi) QueryRouteByID(ctx context.Context, request *routeApi.Request, response *routeApi.Response) error {
 	log.Info("接收到routeApi.QueryRouteByID请求")
-	token := request.Header["Authorization"].GetValues()[0]
-	if token == "" {
-		return errors.New("need login")
+	if _, ok := request.Header["Authorization"]; !ok {
+		return ResponseEmptyToken(ctx, response)
 	}
-	token = strings.TrimPrefix(token, "Bearer ")
-	_, err := jwt.ParseToken(token)
+	err := jwt.CheckToken(request.Header["Authorization"].GetValues()[0])
 	if err != nil {
-		return err
+		return ResponseAuthFailed(ctx, err, response)
 	}
-	// @TODO request.Get["route_id"]能否获取到参数
 	if _, ok := request.Get["route_id"]; !ok {
-		response.StatusCode = WrongArgs
-		return errors.New("参数异常")
+		return ResponseBadRequest(ctx, errors.New("wrong args"), response)
 	}
 	// 获取route id
 	routeID, err := strconv.ParseInt(request.Get["route_id"].Values[0], 10, 64)
 	if err != nil {
-		logger.Error(err)
-		return err
+		return ResponseBadRequest(ctx, errors.New("wrong args"), response)
 	}
 	routeInfo, err := r.RouteService.QueryRouteByID(ctx, &route.RRouteID{Id: routeID})
 	if err != nil {
-		logger.Error(err)
-		return err
+		return ResponseServerError(ctx, err, response)
 	}
-	response.StatusCode = Succeed
-	bytes, _ := json.Marshal(routeInfo)
-	response.Body = string(bytes)
-	return nil
+	return ResponseSucceed(ctx, routeInfo, response)
 }
 
 func (r *RouteApi) AddRoute(ctx context.Context, request *routeApi.Request, response *routeApi.Response) error {
 	log.Info("接收到routeApi.AddRoute请求")
-	token := request.Header["Authorization"].GetValues()[0]
-	if token == "" {
-		return errors.New("need login")
+	if _, ok := request.Header["Authorization"]; !ok {
+		return ResponseEmptyToken(ctx, response)
 	}
-	token = strings.TrimPrefix(token, "Bearer ")
-	_, err := jwt.ParseToken(token)
+	err := jwt.CheckToken(request.Header["Authorization"].GetValues()[0])
 	if err != nil {
-		return err
+		return ResponseAuthFailed(ctx, err, response)
 	}
 	routeInfo := &route.RRouteInfo{}
 	err = json.Unmarshal([]byte(request.Body), routeInfo)
 	if err != nil {
-		response.StatusCode = 403
-		response.Body = err.Error()
-		return err
+		return ResponseBadRequest(ctx, err, response)
 	}
 	routeID, err := r.RouteService.AddRoute(ctx, routeInfo)
 	if err != nil {
-		return err
+		return ResponseServerError(ctx, err, response)
 	}
-	response.StatusCode = Succeed
-	bytes, _ := json.Marshal(routeID)
-	response.Body = string(bytes)
-	return nil
+	return ResponseSucceed(ctx, routeID, response)
 }
 
 func (r *RouteApi) DeleteRoute(ctx context.Context, request *routeApi.Request, response *routeApi.Response) error {
 	log.Info("接收到routeApi.QueryRouteByID请求")
-	token := request.Header["Authorization"].GetValues()[0]
-	if token == "" {
-		return errors.New("need login")
+	if _, ok := request.Header["Authorization"]; !ok {
+		return ResponseEmptyToken(ctx, response)
 	}
-	token = strings.TrimPrefix(token, "Bearer ")
-	_, err := jwt.ParseToken(token)
+	err := jwt.CheckToken(request.Header["Authorization"].GetValues()[0])
 	if err != nil {
-		return err
+		return ResponseAuthFailed(ctx, err, response)
 	}
 	if _, ok := request.Get["route_id"]; !ok {
-		response.StatusCode = WrongArgs
-		return errors.New("参数异常")
+		return ResponseBadRequest(ctx, errors.New("wrong args"), response)
 	}
 	routeID, err := strconv.ParseInt(request.Get["route_id"].Values[0], 10, 64)
 	if err != nil {
-		logger.Error(err)
-		return err
+		return ResponseBadRequest(ctx, errors.New("wrong args"), response)
 	}
 	responseInfo, err := r.RouteService.DeleteRoute(ctx, &route.RRouteID{Id: routeID})
 	if err != nil {
-		logger.Error(err)
-		return err
+		return ResponseServerError(ctx, err, response)
 	}
-	response.StatusCode = Succeed
-	bytes, _ := json.Marshal(responseInfo)
-	response.Body = string(bytes)
-	return nil
+	return ResponseSucceed(ctx, responseInfo, response)
 }
 
 func (r *RouteApi) UpdateRoute(ctx context.Context, request *routeApi.Request, response *routeApi.Response) error {
 	log.Info("接收到routeApi.QueryRouteByID请求")
-	token := request.Header["Authorization"].GetValues()[0]
-	if token == "" {
-		return errors.New("need login")
+	if _, ok := request.Header["Authorization"]; !ok {
+		return ResponseEmptyToken(ctx, response)
 	}
-	token = strings.TrimPrefix(token, "Bearer ")
-	_, err := jwt.ParseToken(token)
+
+	err := jwt.CheckToken(request.Header["Authorization"].GetValues()[0])
 	if err != nil {
-		return err
+		return ResponseAuthFailed(ctx, err, response)
 	}
+
 	routeInfo := &route.RRouteInfo{}
 	err = json.Unmarshal([]byte(request.Body), routeInfo)
 	if err != nil {
-		response.StatusCode = 403
-		response.Body = err.Error()
-		return err
+		return ResponseBadRequest(ctx, err, response)
 	}
+
 	routeID, err := r.RouteService.UpdateRoute(ctx, routeInfo)
 	if err != nil {
-		return err
+		return ResponseServerError(ctx, err, response)
 	}
-	response.StatusCode = Succeed
-	bytes, _ := json.Marshal(routeID)
-	response.Body = string(bytes)
-	return nil
+
+	return ResponseSucceed(ctx, routeID, response)
 }
 
 func (r *RouteApi) Call(ctx context.Context, request *routeApi.Request, response *routeApi.Response) error {
 	log.Info("接收到routeApi.QueryRouteByID请求")
-	token := request.Header["Authorization"].GetValues()[0]
-	if token == "" {
-		return errors.New("need login")
+	if _, ok := request.Header["Authorization"]; !ok {
+		return ResponseEmptyToken(ctx, response)
 	}
-	token = strings.TrimPrefix(token, "Bearer ")
-	_, err := jwt.ParseToken(token)
+
+	err := jwt.CheckToken(request.Header["Authorization"].GetValues()[0])
 	if err != nil {
-		return err
+		return ResponseAuthFailed(ctx, err, response)
 	}
 	allRoute, err := r.RouteService.QueryAllRoute(ctx, &route.RequestQueryAll{})
 	if err != nil {
-		logger.Error(err)
-		return err
+		return ResponseServerError(ctx, err, response)
 	}
-	response.StatusCode = 200
-	bytes, _ := json.Marshal(allRoute)
-	response.Body = string(bytes)
-	return nil
+	return ResponseSucceed(ctx, allRoute, response)
 }
